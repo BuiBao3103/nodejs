68656d7abac88cfba4a76529195d8085b4fa776a
BuiBao3103 <hongbao2003@gmail.com>
2023-07-29 16:53:08 +0700

    build: add eslint

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotend = require('dotenv')
 const app = require('./app')
-dotend.config({path:'./config.env'})
+dotend.config({ path: './config.env' })
 
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 })
+const x = 10
