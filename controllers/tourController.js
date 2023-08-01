78408fd0df37a985e235387d5524ed3b8b9bea59
BuiBao3103 <hongbao2003@gmail.com>
2023-08-01 22:15:18 +0700

    feat: get all tour or one tour from database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,24 +1,43 @@
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    })
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find()
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+
 }
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id)
+        // const tours = await Tour.findOne({_id:req.params.id})
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
     const id = req.params.id * 1
-    // const tour = tours.find(el => el.id === id)
-    // res.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         tour
-    //     }
-    // })
+
 }
 exports.createTour = async (req, res) => {
     console.log(req.body)
