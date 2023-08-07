3f8254ec6634f1b34d86d3477db88e6759583af6
BuiBao3103 <hongbao2003@gmail.com>
2023-08-07 18:32:27 +0700

    refactor: refactor handling global error

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const app = express()
@@ -6,24 +8,9 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
-app.all('*', (req, res) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server!`
-    // })
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-    err.status = 'fail'
-    err.statusCode = 404
-    next(err)
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-})
+app.use(globalErrorHandler)
 module.exports = app
