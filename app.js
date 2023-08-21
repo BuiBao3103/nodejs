a39de227eb17034d2816ffe979967ee123c8d505
BuiBao3103 <hongbao2003@gmail.com>
2023-08-22 01:52:03 +0700

    feat: Data Sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
@@ -17,6 +20,10 @@ const limiter = rateLimit({
 app.use('/api', limiter)
 //Body parser, reading data from body into req body
 app.use(express.json({ limit: '10kb' }))
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+//Data sanitization against XSS
+app.use(xss())
 //Serving static files
 app.use(express.static(`${__dirname}/public`))
 //Routes
