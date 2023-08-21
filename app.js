a8a9f0d2d98548005cd7769b9dc414be2e9f1fcd
BuiBao3103 <hongbao2003@gmail.com>
2023-08-22 00:26:00 +0700

    feat: Implementing Rate Limiting

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const app = express()
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour!'
+})
+app.use('/api', limiter)
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter)
