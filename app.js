cd689302973505346bdec31fd0caa963bc47d7e1
BuiBao3103 <hongbao2003@gmail.com>
2023-08-22 00:38:11 +0700

    feat: Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const app = express()
+//Set security HTTP headers
+app.use(helmet())
+//Limit requests from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many request from this IP, please try again in an hour!'
 })
 app.use('/api', limiter)
-app.use(express.json())
+//Body parser, reading data from body into req body
+app.use(express.json({ limit: '10kb' }))
+//Serving static files
 app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
