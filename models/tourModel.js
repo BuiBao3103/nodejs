1adc86f682995cd8e1c6d8fad82490f3be7cb0fa
BuiBao3103 <hongbao2003@gmail.com>
2023-08-23 18:39:52 +0700

    feat: Modelling Tour Guides Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./userModel')
 // const slugify = require('slugify');
 // const validator = require('validator');
 
@@ -100,7 +101,8 @@ const tourSchema = new mongoose.Schema(
                 description: String,
                 day: Number
             }
-        ]
+        ],
+        guides: Array
     },
     {
         toJSON: { virtuals: true },
@@ -118,6 +120,11 @@ tourSchema.virtual('durationWeeks').get(function () {
 //     next();
 // });
 
+tourSchema.pre('save', async function (next) {
+    const guidesPromises = this.guides.map(async id => await User.findById(id))
+    this.guides = await Promise.all(guidesPromises)
+    next()
+})
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
 //   next();
