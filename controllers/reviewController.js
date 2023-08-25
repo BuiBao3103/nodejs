a2bcff0d8d27c316fea52157af7e87ac0b11d6c1
BuiBao3103 <hongbao2003@gmail.com>
2023-08-26 02:35:23 +0700

    feat: Adding a Nested GET Endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,7 +15,9 @@ exports.createReview = catchAsync(async (req, res) => {
 })
 
 exports.getAllReviews = catchAsync(async (req, res) => {
-    const reviews = await Review.find()
+    let filter = {}
+    if (req.params.tourId) filter = { 'tour': req.params.tourId }
+    const reviews = await Review.find(filter)
     res.status(200).json({
         status: 'success',
         result: reviews.length,
