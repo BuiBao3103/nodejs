69884ec3d61bcf6511c3feaa3e0ef36197b5665b
BuiBao3103 <hongbao2003@gmail.com>
2023-08-26 02:21:31 +0700

    feat: Virtual Populate Tours and Reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.id);
+	const tour = await Tour.findById(req.params.id).populate('reviews');
 	// const tours = await Tour.findOne({_id:req.params.id})
 
 	if (!tour) {
