c4446cc17f7adf5f1ede9295082a50cf25211e6f
BuiBao3103 <hongbao2003@gmail.com>
2023-08-28 23:27:13 +0700

    feat: change Model in create User and  Review(Tour to User,Review)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -27,8 +27,8 @@ const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await Tour.create(reviews);
-    await Tour.create(users);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
