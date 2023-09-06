d15348097926e21efc5d5351e3cb52dbf2ffbf33
BuiBao3103 <hongbao2003@gmail.com>
2023-09-06 23:02:00 +0700

    feat: Building the Login Screen

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -4,4 +4,5 @@ const route = express.Router()
 
 route.get('/', viewsController.getOverview)
 route.get('/tour/:slug', viewsController.getTour)
+route.get('/login', viewsController.getLoginForm)
 module.exports = route
