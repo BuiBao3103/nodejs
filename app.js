c6102a3ee6c0edebc1028b8f35ddac356eb93eab
BuiBao3103 <hongbao2003@gmail.com>
2023-09-08 18:24:33 +0700

    feat: Logging in Users with Our API - Part 1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
+const cookieParser = require('cookie-parser')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -21,7 +22,17 @@ app.set('views', path.join(__dirname, 'views'))
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')))
 //Set security HTTP headers
-app.use(helmet())
+app.use(helmet(
+    {
+        contentSecurityPolicy:
+        {
+            useDefaults: true,
+            directives:
+            {
+                'script-src': ["'self'", "https://cdnjs.cloudflare.com"]
+            }
+        }
+    }))
 //Limit requests from same API
 const limiter = rateLimit({
     max: 100,
@@ -31,6 +42,7 @@ const limiter = rateLimit({
 app.use('/api', limiter)
 //Body parser, reading data from body into req body
 app.use(express.json({ limit: '10kb' }))
+app.use(cookieParser())
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize())
 //Data sanitization against XSS
@@ -46,7 +58,10 @@ app.use(hpp({
         'price']
 }))
 
-
+app.use((req, res, next) => {
+    console.log(req.cookies)
+    next()
+})
 //ROUTES
 app.use('/', viewsRoute)
 app.use('/api/v1/tours', tourRouter)
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const viewsController = require('./../controllers/viewsController')
 const route = express.Router()
+const authController = require('./../controllers/authController')
 
 route.get('/', viewsController.getOverview)
-route.get('/tour/:slug', viewsController.getTour)
+route.get('/tour/:slug', authController.protect, viewsController.getTour)
 route.get('/login', viewsController.getLoginForm)
 module.exports = route
